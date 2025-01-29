fdc2893d9773a72ffaaf729bee8868449163acbd
Sergio2490 <merido2490@gmail.com>
2025-01-30 00:37:59 +0300

    Add game cycle

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ window_size = (800, 600)  #перем. c размерfvb окна: ширина,
 screen = pygame.display.set_mode(window_size) #создаём окно
 pygame.display.set_caption("Тестовый проект") #название окна
 
+run = True
+while run:  #НАЧАЛО БЕСКОНЕЧ.ИГРОВОГО ЦИКЛА. пОКА RUN = истина
+    for event in pygame.event.get(): #проходим. по всем событиям
+        if event.type == pygame.QUIT: #если событие = крестик (чтобы прг не зависала при наж на крестик, а сразу завершалась)
+            run = False #то run = ложь - конец игр цикла
+
+
