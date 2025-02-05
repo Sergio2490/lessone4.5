32eef9cdb42976ebe7b91ec02bda9e7e7ce1f987
Sergio2490 <merido2490@gmail.com>
2025-02-06 01:23:00 +0300

    add move_keys

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ pygame.display.set_caption("Тестовый проект") #название о
 image = pygame.image.load("picPython.png") #загружаем наше изображение
 image_rect = image.get_rect()  # загружаем нашу рамку в отдельную переменную
 
-speed = 5 # скорость перемещ картинки - на сколько она б. перемещ за одно нажатие на клавишу
+speed = 1 # скорость перемещ картинки - на сколько она б. перемещ за одно нажатие на клавишу
 
 
 run = True
@@ -18,6 +18,17 @@ while run:  #НАЧАЛО БЕСКОНЕЧ.ИГРОВОГО ЦИКЛА. пОКА
     for event in pygame.event.get(): #проходим. по всем событиям
         if event.type == pygame.QUIT: #если событие = крестик (чтобы прг не зависала при наж на крестик, а сразу завершалась)
             run = False #то run = ложь - конец игр цикла
+
+    keys = pygame.key.get_pressed()  # сохр в перем-й нажатую клавишу
+    if keys[pygame.K_LEFT]:
+        image_rect.x -= speed  #уменьш на 5 коорд-ту Х нашей рамки, двигаю наше изобр-е влево на 5
+    if keys[pygame.K_RIGHT]:
+        image_rect.x += speed
+    if keys[pygame.K_UP]:
+        image_rect.y -= speed  #стрелка вверх - уменьш коорд у по вертикали на 5
+    if keys[pygame.K_DOWN]:
+        image_rect.y += speed
+
     screen.fill((0,0,0)) #заливает фон окна черным цветом. Двойные скобки - тк передаем кортеж
     screen.blit(image, image_rect)  #эта ф-я отрисовывает и изобр., и его рамку
 
