971cedf1f8c3fc9a2752b555192fb858e7c80971
Sergio2490 <merido2490@gmail.com>
2025-02-08 23:33:03 +0300

    add move mouse

--- main1_mouse.py
+++ main1_mouse.py
@@ -1,4 +1,4 @@
-#example pygame Перемещение изображения с помощью стрелок
+#example pygame Перемещение изображения с помощью МЫШКИ
 
 import pygame
 pygame.init()  #эта ф-я авт-ки иниц-т все необходимые модули pygame. Без нее ничего раб не б.
@@ -10,24 +10,18 @@ pygame.display.set_caption("Тестовый проект") #название о
 image = pygame.image.load("picPython.png") #загружаем наше изображение
 image_rect = image.get_rect()  # загружаем нашу рамку в отдельную переменную
 
-speed = 1 # скорость перемещ картинки - на сколько она б. перемещ за одно нажатие на клавишу
-
 
 run = True
 while run:  #НАЧАЛО БЕСКОНЕЧ.ИГРОВОГО ЦИКЛА. пОКА RUN = истина
     for event in pygame.event.get(): #проходим. по всем событиям
         if event.type == pygame.QUIT: #если событие = крестик (чтобы прг не зависала при наж на крестик, а сразу завершалась)
             run = False #то run = ложь - конец игр цикла
+        if event.type == pygame.MOUSEMOTION: #отслеживаем движение мыши
+            mouseX, mouseY = pygame.mouse.get_pos() #в две перем-е сохранили коорд х и у нашей мышки
+            image_rect.x = mouseX - 30 # передаем тек знач-е коорд Х мышки рамке изображения
+            image_rect.y = mouseY - 30 # -30 - чт курсор был по центру, смещаем само изобр на -30 относит курсора мыши
 
-    keys = pygame.key.get_pressed()  # сохр в перем-й нажатую клавишу
-    if keys[pygame.K_LEFT]:
-        image_rect.x -= speed  #уменьш на 5 коорд-ту Х нашей рамки, двигаю наше изобр-е влево на 5
-    if keys[pygame.K_RIGHT]:
-        image_rect.x += speed
-    if keys[pygame.K_UP]:
-        image_rect.y -= speed  #стрелка вверх - уменьш коорд у по вертикали на 5
-    if keys[pygame.K_DOWN]:
-        image_rect.y += speed
+
 
     screen.fill((0,0,0)) #заливает фон окна черным цветом. Двойные скобки - тк передаем кортеж
     screen.blit(image, image_rect)  #эта ф-я отрисовывает и изобр., и его рамку
@@ -35,4 +29,3 @@ while run:  #НАЧАЛО БЕСКОНЕЧ.ИГРОВОГО ЦИКЛА. пОКА
     pygame.display.flip()  #обновл.содержимое экрана. М исп и update() - весь экран или часть его.
 
 pygame.quit()
-
